4f373188977e48f3eafb9c86845cd2459c6a4ddf
Luke Penner <lukepenner@hotmail.ca>
2021-05-25 18:27:23 -0700

    ex6 done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# @store1 = Store.where(id: 1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Blow", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Sagat", hourly_rate: 60)
+@store2.employees.create(first_name: "Jenny", last_name: "Law", hourly_rate: 60)
+@store2.employees.create(first_name: "Ashton", last_name: "Kutcher", hourly_rate: 60)
+@store2.employees.create(first_name: "Sarah", last_name: "Palin", hourly_rate: 60)
